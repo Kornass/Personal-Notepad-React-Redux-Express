cc31d4713bf0f35f5b7ac225fafd8de85510a759
Paweł Kornas <kornasnorge@gmail.com>
2023-04-01 21:31:22 +0200

    heroku setup

--- server/index.js
+++ server/index.js
@@ -1,3 +1,4 @@
+const path = require("path");
 const express = require("express");
 const colors = require("colors");
 const dotenv = require("dotenv").config();
@@ -14,6 +15,19 @@ connectDB();
 
 app.use("/api/posts", require("./routes/postRoute"));
 app.use("/api/users", require("./routes/userRoutes"));
+// Server frontend
+if (process.env.NODE_ENV === "production") {
+  // we are serving our static build folder from react npm run build
+  app.use(express.static(path.join(__dirname, "../client/build")));
+  // loading index.html that is in the build folder
+  app.get("*", (req, res) =>
+    res.sendFile(__dirname, "../", "client", "build", "index.html")
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.status(200).json({ message: "Welcome" });
+  });
+}
 app.use(errorHandler);
 
 app.listen(PORT, () => console.log(`Server running on ${PORT}`));
