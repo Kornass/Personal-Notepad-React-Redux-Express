14ae1ea58a8603124a847ee3aba2d519a7f419c0
Paweł Kornas <kornasnorge@gmail.com>
2024-07-29 16:36:56 +0200

    first deployment configurations

--- server/index.js
+++ server/index.js
@@ -6,6 +6,9 @@ const { errorHandler } = require("./middleware/errorMiddleware");
 const connectDB = require("./config/db");
 const PORT = process.env.PORT || 6000;
 const app = express();
+const cors = require("cors");
+
+app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
