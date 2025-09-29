9a5ae6e58c64b30646a4cc236eb6a96f7e463a33
Pawel Kornas <kornasnorge@gmail.com>
2025-09-29 03:12:34 +0200

    proxy

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const colors = require("colors");
 const dotenv = require("dotenv").config();
 const { errorHandler } = require("./middleware/errorMiddleware");
 const connectDB = require("./config/db");
-const PORT = process.env.PORT || 6000;
+const PORT = process.env.PORT || 5000;
 const app = express();
 const cors = require("cors");
 
