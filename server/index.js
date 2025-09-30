a40568d019870106e7cda89b7cd2233e5305cc14
Pawel Kornas <kornasnorge@gmail.com>
2025-09-30 02:02:14 +0200

    cleanup

--- server/index.js
+++ server/index.js
@@ -1,7 +1,4 @@
-const path = require("path");
 const express = require("express");
-const colors = require("colors");
-const dotenv = require("dotenv").config();
 const { errorHandler } = require("./middleware/errorMiddleware");
 const connectDB = require("./config/db");
 const PORT = process.env.PORT || 5000;
@@ -14,24 +11,15 @@ app.use((req, _res, next) => {
 });
 
 const corsOptions = {
-  origin: ["https://personal-notepad-react-redux.vercel.app"], // exact match
+  origin: ["https://personal-notepad-react-redux.vercel.app"],
   methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
-  // credentials: true, // only if you actually use cookies
   optionsSuccessStatus: 204,
 };
 
-app.use(cors(corsOptions)); // must be BEFORE routes
-app.options("*", cors(corsOptions)); // handle preflight globally
+app.use(cors(corsOptions));
+app.options("*", cors(corsOptions));
 
-// app.use(
-//   cors({
-//     origin: "https://personal-notepad-react-redux.vercel.app",
-//   })
-// );
-// app.options("/*", (_, res) => {
-//   res.sendStatus(200);
-// });
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
@@ -43,9 +31,7 @@ app.use("/api/users", require("./routes/userRoutes"));
 app.use(errorHandler);
 
 if (process.env.VERCEL) {
-  // On Vercel: export a handler, DO NOT listen
   module.exports = (req, res) => app(req, res);
 } else {
-  // Local dev:
   app.listen(PORT, () => console.log(`Server running on ${PORT}`));
 }
